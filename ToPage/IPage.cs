e45ed28a660200f660d506390669359fdab30c4b
collenirwin <collenirwin@gmail.com>
2020-10-10 16:52:52 -0400

    add proj start/page models

---
+++ ToPage/IPage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ToPage
+{
+    public interface IPage
+    {
+        int PageNumber { get; }
+    }
+
+    public interface IPage<T> : IPage
+    {
+        IEnumerable<T> Items { get; }
+    }
+}
---
+++ ToPage/Page.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ToPage
+{
+    public class Page<T> : IPage<T>
+    {
+        public IEnumerable<T> Items { get; }
+
+        public int PageNumber { get; }
+
+        public Page(IEnumerable<T> items, int pageNumber)
+        {
+            Items = items;
+            PageNumber = pageNumber;
+        }
+    }
+}
