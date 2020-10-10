03eec028f4ac94f59866b0ed7c774da176c4fc3a
collenirwin <collenirwin@gmail.com>
2020-10-10 17:13:11 -0400

    add QueryableExtensions, ToPage method

---
+++ ToPage/QueryableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace ToPage
+{
+    public static class QueryableExtensions
+    {
+        public static Page<T> ToPage<T>(this IOrderedQueryable<T> query, int pageNumber, int itemsPerPage)
+        {
+            AssertValidToPageArgs(query, pageNumber, itemsPerPage);
+
+            var items = query
+                .Skip((pageNumber - 1) * itemsPerPage)
+                .Take(itemsPerPage)
+                .ToList();
+
+            return new Page<T>(items, pageNumber);
+        }
+
+        private static void AssertValidToPageArgs<T>(IOrderedQueryable<T> query, int pageNumber, int itemsPerPage)
+        {
+            _ = query ?? throw new ArgumentNullException(nameof(query));
+
+            if (pageNumber < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pageNumber));
+            }
+
+            if (itemsPerPage < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(itemsPerPage));
+            }
+        }
+    }
+}
