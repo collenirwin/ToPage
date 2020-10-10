3c6e4abd2303fb6bb3d907a6b1c59c8ab6ade6d4
collenirwin <collenirwin@gmail.com>
2020-10-10 17:42:56 -0400

    add unit tests ensuring exception throws

--- ToPage.Tests/QueryableTests.cs
+++ ToPage.Tests/QueryableTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Xunit;
 
@@ -33,6 +34,42 @@ namespace ToPage.Tests
 
             // assert
             Assert.Equal(itemsPerPage, page.Items.Count());
+        }
+
+        [Fact]
+        public void Throws_On_Null_Query()
+        {
+            // arrange
+            IOrderedQueryable<int> query = null;
+            int pageNumber = 1;
+            int itemsPerPage = 10;
+
+            // act, assert
+            Assert.Throws<ArgumentNullException>(() => _ = query.ToPage(pageNumber, itemsPerPage));
+        }
+
+        [Fact]
+        public void Throws_On_PageNumber_Under_1()
+        {
+            // arrange
+            IOrderedQueryable<int> query = GetBasicOrderedQuery();
+            int pageNumber = 0;
+            int itemsPerPage = 10;
+
+            // act, assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => _ = query.ToPage(pageNumber, itemsPerPage));
+        }
+
+        [Fact]
+        public void Throws_On_ItemsPerPage_Under_1()
+        {
+            // arrange
+            IOrderedQueryable<int> query = GetBasicOrderedQuery();
+            int pageNumber = 1;
+            int itemsPerPage = 0;
+
+            // act, assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => _ = query.ToPage(pageNumber, itemsPerPage));
         }
 
         private IOrderedQueryable<int> GetBasicOrderedQuery()
