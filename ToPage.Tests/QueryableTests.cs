511c36e306cb66aac2f2fa58b5407bba133ac6c2
collenirwin <collenirwin@gmail.com>
2020-10-10 17:53:51 -0400

    add exhaustive page item correctness test

--- ToPage.Tests/QueryableTests.cs
+++ ToPage.Tests/QueryableTests.cs
@@ -102,6 +102,24 @@ namespace ToPage.Tests
             Assert.Empty(page.Items);
         }
 
+        [Fact]
+        public void All_Pages_Have_Correct_Items()
+        {
+            // arrange
+            var query = GetBasicOrderedQuery();
+            int itemsPerPage = 1;
+
+            // act, assert
+            for (int x = 1; x <= 100; x++)
+            {
+                var page = query.ToPage(pageNumber: x, itemsPerPage);
+                Assert.Single(page.Items, x);
+            }
+
+            var emptyPage = query.ToPage(pageNumber: 101, itemsPerPage);
+            Assert.Empty(emptyPage.Items);
+        }
+
         private IOrderedQueryable<int> GetBasicOrderedQuery()
             => Enumerable.Range(1, 100)
                 .AsQueryable()
