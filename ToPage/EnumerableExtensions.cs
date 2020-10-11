e8bb798407b583180441dcaeb94ac6758d321751
collenirwin <collenirwin@gmail.com>
2020-10-10 21:17:43 -0400

    add Enumerable.ToPageWithCounts method

--- ToPage/EnumerableExtensions.cs
+++ ToPage/EnumerableExtensions.cs
@@ -34,5 +34,33 @@ namespace ToPage
 
             return new Page<T>(items, pageNumber);
         }
+
+        /// <summary>
+        /// Creates a <see cref="PageWithCounts{T}"/> from the enumerable.
+        /// </summary>
+        /// <typeparam name="T">The enumerable's item type.</typeparam>
+        /// <param name="values">Enumerable to build the page from.</param>
+        /// <param name="pageNumber">The 1-based page number to get.</param>
+        /// <param name="itemsPerPage">The number of items on the page.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="values"/> or  is <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when <paramref name="pageNumber"/> or <paramref name="itemsPerPage"/> is less than 1.
+        /// </exception>
+        /// <exception cref="OverflowException">
+        /// Thrown when <paramref name="values"/> contains more than <see cref="int.MaxValue"/> items.
+        /// </exception>
+        /// <returns>The specified page from the enumerable.</returns>
+        public static PageWithCounts<T> ToPageWithCounts<T>(this IEnumerable<T> values,
+            int pageNumber, int itemsPerPage)
+        {
+            var page = ToPage(values, pageNumber, itemsPerPage);
+
+            int itemCount = values.Count();
+            int pageCount = (int)Math.Ceiling((double)itemCount / itemsPerPage);
+
+            return new PageWithCounts<T>(page.Items, page.PageNumber, itemCount, pageCount);
+        }
     }
 }
