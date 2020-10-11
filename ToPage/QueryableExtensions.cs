8506a654b6ee8c168de855b8007c9f689fbff46d
collenirwin <collenirwin@gmail.com>
2020-10-10 20:39:31 -0400

    move AssertValidToPageArgs to new ThrowUtils class

--- ToPage/QueryableExtensions.cs
+++ ToPage/QueryableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ToPage.Utils;
 
 namespace ToPage
 {
@@ -44,30 +45,13 @@ namespace ToPage
         public static Page<T> ToPage<T>(this IOrderedQueryable<T> query, int pageNumber, int itemsPerPage,
             Func<IQueryable<T>, IEnumerable<T>> itemsEnumerator)
         {
-            AssertValidToPageArgs(query, pageNumber, itemsPerPage, itemsEnumerator);
+            ThrowUtils.AssertValidToPageArgs(query, pageNumber, itemsPerPage, itemsEnumerator);
 
             var items = itemsEnumerator(query
                 .Skip((pageNumber - 1) * itemsPerPage)
                 .Take(itemsPerPage));
 
             return new Page<T>(items, pageNumber);
-        }
-
-        private static void AssertValidToPageArgs<T>(IOrderedQueryable<T> query, int pageNumber, int itemsPerPage,
-            Func<IQueryable<T>, IEnumerable<T>> itemsEnumerator)
-        {
-            _ = query ?? throw new ArgumentNullException(nameof(query));
-            _ = itemsEnumerator ?? throw new ArgumentNullException(nameof(itemsEnumerator));
-
-            if (pageNumber < 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(pageNumber));
-            }
-
-            if (itemsPerPage < 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(itemsPerPage));
-            }
         }
     }
 }
---
+++ ToPage/Utils/ThrowUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToPage.Utils
+{
+    internal static class ThrowUtils
+    {
+        public static void AssertValidToPageArgs<T>(IOrderedQueryable<T> query, int pageNumber, int itemsPerPage,
+            Func<IQueryable<T>, IEnumerable<T>> itemsEnumerator)
+        {
+            _ = query ?? throw new ArgumentNullException(nameof(query));
+            _ = itemsEnumerator ?? throw new ArgumentNullException(nameof(itemsEnumerator));
+
+            AssertValidPageNumber(pageNumber);
+            AssertValidItemsPerPage(itemsPerPage);
+        }
+
+        public static void AssertValidPageNumber(int pageNumber)
+        {
+            if (pageNumber < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pageNumber));
+            }
+        }
+
+        public static void AssertValidItemsPerPage(int itemsPerPage)
+        {
+            if (itemsPerPage < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(itemsPerPage));
+            }
+        }
+    }
+}
