ecd3ce83730824b22ae36b2506672396cbaf4f7b
collenirwin <collenirwin@gmail.com>
2021-01-16 14:09:53 -0500

    Page.Empty, EmptyWithCounts return interface over impl

--- ToPage/Page.static.cs
+++ ToPage/Page.static.cs
@@ -8,18 +8,18 @@ namespace ToPage
     public static class Page
     {
         /// <summary>
-        /// Creates an empty <see cref="Page{T}"/>.
+        /// Creates an empty <see cref="IPage{T}"/>.
         /// </summary>
         /// <typeparam name="T">Type of the items on the page.</typeparam>
-        /// <returns>An empty <see cref="Page{T}"/>.</returns>
-        public static Page<T> Empty<T>() => new Page<T>(items: Enumerable.Empty<T>(), pageNumber: 1);
+        /// <returns>An empty <see cref="IPage{T}"/>.</returns>
+        public static IPage<T> Empty<T>() => new Page<T>(items: Enumerable.Empty<T>(), pageNumber: 1);
 
         /// <summary>
-        /// Creates an empty <see cref="PageWithCounts{T}"/>.
+        /// Creates an empty <see cref="IPageWithCounts{T}"/>.
         /// </summary>
         /// <typeparam name="T">Type of the items on the page.</typeparam>
-        /// <returns>An empty <see cref="PageWithCounts{T}"/>.</returns>
-        public static PageWithCounts<T> EmptyWithCounts<T>()
+        /// <returns>An empty <see cref="IPageWithCounts{T}"/>.</returns>
+        public static IPageWithCounts<T> EmptyWithCounts<T>()
             => new PageWithCounts<T>(items: Enumerable.Empty<T>(), pageNumber: 1, itemCount: 0, pageCount: 0);
     }
 }
